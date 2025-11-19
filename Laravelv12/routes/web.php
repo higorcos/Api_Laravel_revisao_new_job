a16ed22200ee820d265824f97c96deaa7027d94a
Higor Costa <higorcosta2001@outlook.com>
2025-11-18 21:26:58 -0300

    delete

---
+++ Laravelv12/routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::get('/', function () {
+    return Inertia::render('Welcome');
+})->name('home');
+
+Route::get('a/', function (){
+    return 'Hellow Word';
+});
